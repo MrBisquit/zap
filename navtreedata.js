9e4faa178e2f2c0e3b2766c64d3b238853c83a29
MrBisquit <69386111+MrBisquit@users.noreply.github.com>
2026-06-27 22:03:19 +0000

    Deploying to gh-pages from @ MrBisquit/zap@9606a3e4f0a14f0f1be2b676fa4fc4290073dce7 🚀

--- navtreedata.js
+++ navtreedata.js
@@ -25,10 +25,20 @@
 var NAVTREE =
 [
   [ "ZAP", "index.html", [
+    [ "Changelog", "dd/d44/md_src_2lsp_2vscode_2zap_2CHANGELOG.html", [
+      [ "[0.0.1] - 2026-03-16", "dd/d44/md_src_2lsp_2vscode_2zap_2CHANGELOG.html#autotoc_md12", [
+        [ "Added", "dd/d44/md_src_2lsp_2vscode_2zap_2CHANGELOG.html#autotoc_md13", null ],
+        [ "Changed", "dd/d44/md_src_2lsp_2vscode_2zap_2CHANGELOG.html#autotoc_md14", null ],
+        [ "Removed", "dd/d44/md_src_2lsp_2vscode_2zap_2CHANGELOG.html#autotoc_md15", null ]
+      ] ]
+    ] ],
     [ "Namespaces", "namespaces.html", [
       [ "Namespace List", "namespaces.html", "namespaces_dup" ],
       [ "Namespace Members", "namespacemembers.html", [
-        [ "All", "namespacemembers.html", null ],
+        [ "All", "namespacemembers.html", "namespacemembers_dup" ],
+        [ "Functions", "namespacemembers_func.html", "namespacemembers_func" ],
+        [ "Variables", "namespacemembers_vars.html", null ],
+        [ "Typedefs", "namespacemembers_type.html", null ],
         [ "Enumerations", "namespacemembers_enum.html", null ]
       ] ]
     ] ],
@@ -39,16 +49,23 @@ var NAVTREE =
       [ "Class Members", "functions.html", [
         [ "All", "functions.html", "functions_dup" ],
         [ "Functions", "functions_func.html", "functions_func" ],
-        [ "Variables", "functions_vars.html", "functions_vars" ]
+        [ "Variables", "functions_vars.html", "functions_vars" ],
+        [ "Typedefs", "functions_type.html", null ],
+        [ "Enumerations", "functions_enum.html", null ],
+        [ "Enumerator", "functions_eval.html", null ],
+        [ "Related Symbols", "functions_rela.html", null ]
       ] ]
     ] ],
     [ "Files", "files.html", [
       [ "File List", "files.html", "files_dup" ],
       [ "File Members", "globals.html", [
-        [ "All", "globals.html", null ],
+        [ "All", "globals.html", "globals_dup" ],
         [ "Functions", "globals_func.html", null ],
+        [ "Variables", "globals_vars.html", null ],
+        [ "Typedefs", "globals_type.html", null ],
         [ "Enumerations", "globals_enum.html", null ],
-        [ "Enumerator", "globals_eval.html", null ]
+        [ "Enumerator", "globals_eval.html", null ],
+        [ "Macros", "globals_defs.html", null ]
       ] ]
     ] ]
   ] ]
@@ -57,12 +74,24 @@ var NAVTREE =
 var NAVTREEINDEX =
 [
 "annotated.html",
-"d2/d6e/classToken.html#a754269b5bd4df4d60520f4b537a07300",
-"d4/da0/classzir_1_1BoundIRGenerator.html#a191f154271fba1cede2d0e221092e63b",
-"d8/d6d/classzir_1_1LoadInst.html#ab3a73a2e50fe741c0bb1d13bf68ab6d0",
-"db/d85/classsema_1_1BoundLiteral.html#a2b69026e358b10280e615c28ad0009ce",
-"dd/d86/classFunCall.html#ad8b89e6522c61b5e62693eb91b920810",
-"df/da0/classsema_1_1BoundFunctionCall.html"
+"d0/df9/classsema_1_1BoundVisitor.html#a4a9487156c524abc8a0834a0ce4605cb",
+"d1/de8/structzap__arc__header__t.html#a30eec0b46f324f38bb1ba57172944d1e",
+"d2/dba/classzap_1_1lsp_1_1JsonObject.html#ae670747c7961073c3c59f960095eab01",
+"d3/d57/classzir_1_1Argument.html#a1547c6df5662eb69f940d35c2e554691",
+"d4/d44/token_8hpp.html#aa520fbf142ba1e7e659590c07da31921a8158fc7dd6dcf9c20a7a9f1e256ec0b5",
+"d5/d23/namespacezap_1_1lsp.html#a15c01a432bc7764c1cdd7f5aabb91935",
+"d6/d7b/classClassDecl.html#abff211c086e8e94927d4e0983bdd86f6",
+"d8/d4b/classsema_1_1BoundStructLiteral.html#a8e628c799d688bea36ad8b3dd934d7fb",
+"d9/d0d/classzap_1_1lsp_1_1Server.html#a8914da35827f80df1c97a29a01f3b5e2",
+"d9/dff/classMemberAccessNode.html",
+"da/de9/classcodegen_1_1LLVMCodeGen.html#a0f07d70f216d99fc77539272f5028ad8",
+"db/d2f/classsema_1_1BoundCompoundTargetLoad.html#a760fbe5cbdf31a5caffe3b4fcd4a2a0e",
+"db/dfa/stdlib_8c.html#a768fbe26f8d9de5a5f64130f84be0d37",
+"dc/d2c/classsema_1_1Binder.html#a5e4cdf252a1ed16bc63855daf6b8c295",
+"dc/dae/classzir_1_1ClassType.html#a0c7b59f769c2874b66e2676db548e327",
+"de/d3e/classsema_1_1BoundIndirectCall.html#ad0de9073109f732817a0c3bb1739bd44",
+"df/d0a/classzir_1_1BinaryInst.html",
+"df/dee/classsema_1_1BoundBinaryExpression.html#a712fca1a27f6668ce1b8edad8b1d49c8"
 ];
 
 var SYNCONMSG = 'click to disable panel synchronization';
